3081b808728441322ad53f294d6d2db07ac6404a
Shaker Kattoula <smkattoula@gmail.com>
2020-08-18 12:51:08 -0700

    create axios get request to fetch TicketList info from database

--- client/src/components/TicketList.js
+++ client/src/components/TicketList.js
@@ -1,5 +1,32 @@
 import React, { Component } from "react";
 import { Table } from "reactstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Ticket = (props) => (
+  <tr>
+    <td>{props.ticket.subject}</td>
+    <td>{props.ticket.description}</td>
+    <td>{props.ticket.category}</td>
+    <td>{props.ticket.priority}</td>
+    <td>{props.ticket.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.ticket._id} style={{ color: "#20c997" }}>
+        Edit
+      </Link>{" "}
+      |{" "}
+      <button
+        style={{ color: "#dc3545", border: "none", background: "transparent" }}
+        href="#"
+        onClick={() => {
+          props.deleteTicket(props.ticket._id);
+        }}
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
+);
 
 export default class TicketList extends Component {
   constructor(props) {
@@ -8,8 +35,36 @@ export default class TicketList extends Component {
     this.state = { tickets: [] };
   }
 
-  // componentDidMount() {}
+  componentDidMount() {
+    axios
+      .get("/api/tickets")
+      .then((res) => {
+        this.setState({ tickets: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
+  deleteTicket = (id) => {
+    axios.delete("/api/tickets/" + id).then((res) => console.log(res.data));
+
+    this.setState({
+      tickets: this.state.tickets.filter((el) => el._id !== id),
+    });
+  };
+
+  ticketList = () => {
+    return this.state.tickets.map((currentTicket) => {
+      return (
+        <Ticket
+          ticket={currentTicket}
+          deleteTicket={this.deleteTicket}
+          key={currentTicket._id}
+        />
+      );
+    });
+  };
   render() {
     return (
       <Table striped>
@@ -19,9 +74,11 @@ export default class TicketList extends Component {
             <th>Description</th>
             <th>Category</th>
             <th>Priority</th>
+            <th>Date</th>
+            <th>Options</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{this.ticketList()}</tbody>
       </Table>
     );
   }
