ce86475c4e20d6ce79f077a17d093b8e59be5e39
Shaker Kattoula <smkattoula@gmail.com>
2021-01-02 16:14:06 -0800

    change navbar spacing

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import AuthOptions from "./AuthOptions";
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  Container,
-} from "reactstrap";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav } from "reactstrap";
 
 class AppNavBar extends Component {
   state = {
@@ -25,15 +18,13 @@ class AppNavBar extends Component {
     return (
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5 navbar">
-          <Container>
-            <NavbarBrand href="/">Ticket Tracker</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <AuthOptions />
-              </Nav>
-            </Collapse>
-          </Container>
+          <NavbarBrand href="/">Ticket Tracker</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <AuthOptions />
+            </Nav>
+          </Collapse>
         </Navbar>
       </div>
     );
